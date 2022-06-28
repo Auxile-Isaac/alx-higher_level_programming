1c7ca57000b7c5e3c3542c8e8b1b3b7d6805b812
Auxile_Isaac <auxile.isaac@gmail.com>
2022-06-28 19:22:25 +0200

    correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    print(f"{c:c}", end="")
+for alphabet in range(ord('a'), ord('z') + 1):
+    if alphabet != ord('e') and alphabet != ord('q'):
+        print(f"{alphabet:c}", end="")
